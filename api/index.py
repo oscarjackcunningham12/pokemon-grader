fa1f63c394a917e32c2da5a810ec58abb345d797
oscarjackcunningham12 <oscarjackcunningham@gmail.com>
2026-05-24 15:11:43 -0400

    updated index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,10 @@
+import sys
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parent.parent
+BACKEND = ROOT / "backend"
+
+sys.path.insert(0, str(ROOT))
+sys.path.insert(0, str(BACKEND))
+
 from backend.app import app
